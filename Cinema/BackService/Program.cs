b9fbc925bd923ecc34a06064d41051dc8c4e8639
Andrey Karpov <karpowandrey13@mail.ru>
2022-02-13 18:22:51 +0300

    test create db

--- Cinema/BackService/Program.cs
+++ Cinema/BackService/Program.cs
@@ -1,6 +1,32 @@
+using Core.Data;
+using Core.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+string connectionString = config["CinemaDb"] ?? "Filename=Cinema.db";
+
+builder.Services.AddDbContext<CinemaContext>(options => options.UseSqlite(connectionString).LogTo(Console.WriteLine));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (CinemaContext db) => {
+    db.Database.EnsureCreated();
+
+    return db.Cinemas.FirstOrDefault();
+});
+
+app.MapGet("/test", (CinemaContext db) => {
+    db.Database.EnsureCreated();
+    db.Add(new Cinema()
+    {
+        Name = "Люксор",
+        Address = "Московское шоссе, барс"
+    });
+
+    db.SaveChanges();
+
+    return Results.Redirect("/");
+});
 
 app.Run();
--- Cinema/Core/Data/CinemaContext.cs
+++ Cinema/Core/Data/CinemaContext.cs
@@ -1,21 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace Core.Data
 {
     public class CinemaContext : DbContext
     {
+        public DbSet<Cinema> Cinemas { get; set; }
+
+        public CinemaContext(DbContextOptions<CinemaContext> options) : base(options)
+        {
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            IConfiguration configuration = new ConfigurationBuilder()
-                .AddEnvironmentVariables()
-                .Build();
+            //IConfiguration configuration = new ConfigurationBuilder()
+            //    .AddEnvironmentVariables()
+            //    .Build();
 
-            string connectionString = configuration["CinemaDb"] ?? "Filename=Cinema.db";
+            //string connectionString = configuration["CinemaDb"] ?? "Filename=Cinema.db";
 
-            optionsBuilder
-                .UseSqlite(connectionString)
-                .LogTo(Console.WriteLine);
+            //optionsBuilder
+            //    .UseSqlite(connectionString)
+            //    .LogTo(Console.WriteLine);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
